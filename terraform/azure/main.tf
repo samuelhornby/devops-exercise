0472ad672ee42050a72d9279c11fe468945355ff
Samuel Hornby <samuel.hornby@handinfun.co.nz>
2019-08-14 11:54:10 +1200

    added sg

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -43,6 +43,8 @@ resource "azurerm_network_security_rule" "ssh_rule" {
   resource_group_name         = azurerm_resource_group.my_group.name
   source_port_range           = "*"
   destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix = "*"
 }
 
 resource "azurerm_public_ip" "my_vm_pub_ip" {
@@ -56,6 +58,7 @@ resource "azurerm_network_interface" "my_nic" {
   location            = azurerm_resource_group.my_group.location
   name                = "my_nic"
   resource_group_name = azurerm_resource_group.my_group.name
+  network_security_group_id = azurerm_network_security_group.my_sg.id
 
   ip_configuration {
     name                          = "myconf"
