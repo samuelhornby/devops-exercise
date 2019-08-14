1df0f5f7743b27405435ee689654cb9760f6e001
Samuel Hornby <samuel.hornby@handinfun.co.nz>
2019-08-14 14:35:26 +1200

    azure kubernetes

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -47,6 +47,20 @@ resource "azurerm_network_security_rule" "ssh_rule" {
   destination_address_prefix = "*"
 }
 
+resource "azurerm_network_security_rule" "web_access_rule" {
+  access                      = "Allow"
+  direction                   = "Inbound"
+  name                        = "web_access"
+  network_security_group_name = azurerm_network_security_group.my_sg.name
+  priority                    = 101
+  protocol                    = "Tcp"
+  resource_group_name         = azurerm_resource_group.my_group.name
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix = "*"
+}
+
 resource "azurerm_public_ip" "my_vm_pub_ip" {
   location            = azurerm_resource_group.my_group.location
   name                = "vm_pub_ip"
@@ -69,40 +83,21 @@ resource "azurerm_network_interface" "my_nic" {
 }
 
 
-# Virtual machines
-resource "azurerm_virtual_machine" "my_vm" {
-  location                         = azurerm_resource_group.my_group.location
-  name                             = "my_vm"
-  network_interface_ids            = [azurerm_network_interface.my_nic.id]
-  resource_group_name              = azurerm_resource_group.my_group.name
-  vm_size                          = "Standard_DS1_v2"
-  delete_os_disk_on_termination    = true
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+# Kubernetes cluster
+resource "azurerm_kubernetes_cluster" "my_cluster" {
+  dns_prefix = "sam"
+  location = azurerm_resource_group.my_group.location
+  name = "my_cluster"
+  resource_group_name = azurerm_resource_group.my_group.name
+  agent_pool_profile {
+    name = "default"
+    vm_size = "Standard_D1_v2"
+    count = 3
+    os_type = "Linux"
+    os_disk_size_gb = 30
   }
-
-  storage_os_disk {
-    create_option     = "FromImage"
-    name              = "my_os_disk"
-    caching           = "ReadWrite"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    admin_username = "sam"
-    computer_name  = "bigdaddyv"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      key_data = file("/home/sam/.ssh/id_rsa.pub")
-      path     = "/home/sam/.ssh/authorized_keys"
-    }
+  service_principal {
+    client_id     = ""
+    client_secret = ""
   }
 }
